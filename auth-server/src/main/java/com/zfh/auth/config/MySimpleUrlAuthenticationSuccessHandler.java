da511e46f8596d00163393d0efab609ed3850506
111333 <111333222>
2019-02-27 16:19:52 +0800

    create MySimpleUrlAuthenticationSuccessHandler class

---
+++ auth-server/src/main/java/com/zfh/auth/config/MySimpleUrlAuthenticationSuccessHandler.java
@@ -0,0 +1,106 @@
+package com.zfh.auth.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.WebAttributes;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.swing.*;
+import java.io.IOException;
+import java.util.Collection;
+
+/**
+ * Spring Security提供的一个组件，它直接负责决定成功验证后要执行的操作
+ *
+ * 除了AuthenticationSuccessHandler接口之外，Spring还为此策略组件提供了合理的默认设置 - AbstractAuthenticationTargetUrlRequestHandler
+ * 和一个简单的实现 - SimpleUrlAuthenticationSuccessHandler。
+ *
+ * @author  zfh
+ * @since 2019/2/13
+ */
+public class MySimpleUrlAuthenticationSuccessHandler
+        implements AuthenticationSuccessHandler {
+
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response, Authentication authentication)
+            throws IOException {
+        // Spring Security Web 提供了在http session中缓存请求的能力，
+        // 也就是HttpSessionRequestCache。HttpSessionRequestCache所保存的9请求必须封装成一个SavedRequest接口对象，实际上，HttpSessionRequestCache总是使用自己的SavedRequest缺省实现DefaultSavedRequest
+        SavedRequest savedRequest = new HttpSessionRequestCache().getRequest(request,response);
+        System.out.println("1");
+
+        handle(request, response, authentication);
+        clearAuthenticationAttributes(request);
+
+    }
+
+    protected void handle(HttpServletRequest request,
+                          HttpServletResponse response, Authentication authentication)
+            throws IOException {
+
+        String targetUrl = determineTargetUrl(authentication);
+
+        if (response.isCommitted()) {
+            logger.debug(
+                    "Response has already been committed. Unable to redirect to "
+                            + targetUrl);
+            return;
+        }
+
+        redirectStrategy.sendRedirect(request, response, targetUrl);
+    }
+
+    protected String determineTargetUrl(Authentication authentication) {
+        boolean isUser = false;
+        boolean isAdmin = false;
+        Collection<? extends GrantedAuthority> authorities
+                = authentication.getAuthorities();
+        for (GrantedAuthority grantedAuthority : authorities) {
+            if (grantedAuthority.getAuthority().equals("ROLE_USER")) {
+                isUser = true;
+                break;
+            } else if (grantedAuthority.getAuthority().equals("ROLE_ADMIN")) {
+                isAdmin = true;
+                break;
+            }
+        }
+
+        if (isUser) {
+            return "/homepage.html";
+        } else if (isAdmin) {
+            return "/console.html";
+        } else {
+            throw new IllegalStateException();
+        }
+    }
+
+    protected void clearAuthenticationAttributes(HttpServletRequest request) {
+        //getSession(boolean create)意思是返回当前reqeust中的HttpSession ，如果当前reqeust中的HttpSession 为null，当create为true，就创建一个新的Session，否则返回null；
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return;
+        }
+        session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+    }
+
+    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+        this.redirectStrategy = redirectStrategy;
+    }
+    protected RedirectStrategy getRedirectStrategy() {
+        return redirectStrategy;
+    }
+}
